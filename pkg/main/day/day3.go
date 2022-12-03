6e653a570d32cb3b9cfb96e5d7aed733f69136a0
Garrett Koontz <garrett@Garretts-MacBook-Air-2.local>
2022-12-03 10:03:15 -0500

    Day 3 part 1

--- pkg/main/day/day3.go
+++ pkg/main/day/day3.go
@@ -7,8 +7,49 @@ import (
 type Day3 struct {
 }
 
-func (d Day3) processInput(input []string) int {
-	return 0
+type sack struct {
+	Compartment1 map[rune]int
+	Compartment2 map[rune]int
+}
+
+func (s *sack) share() rune {
+	for k, _ := range s.Compartment1 {
+		if _, ok := s.Compartment2[k]; ok {
+			return k
+		}
+	}
+	return '@'
+}
+
+func priority(r rune) int {
+	i := int(r)
+	if i <= 90 {
+		return i - 38
+	}
+	return i - 96
+}
+
+func toSet(s string) map[rune]int {
+	set := make(map[rune]int)
+	for _, c := range s {
+		if val, ok := set[c]; ok {
+			set[c] = val + 1
+		}
+		set[c] = 1
+	}
+	return set
+}
+
+func (d Day3) processInput(input []string) []*sack {
+	sacks := make([]*sack, len(input))
+	for i, s := range input {
+		l := len(s)
+		sacks[i] = &sack{
+			Compartment1: toSet(s[0:(l / 2)]),
+			Compartment2: toSet(s[(l / 2):]),
+		}
+	}
+	return sacks
 }
 
 func (d Day3) Run() {
@@ -23,10 +64,15 @@ func (d Day3) Run() {
 	fmt.Println(d.part2(inp))
 }
 
-func (d Day3) part1(i int) int {
-	return 0
+func (d Day3) part1(stacks []*sack) int {
+	p := 0
+	for _, s := range stacks {
+		r := s.share()
+		p += priority(r)
+	}
+	return p
 }
 
-func (d Day3) part2(i int) int {
+func (d Day3) part2(stacks []*sack) int {
 	return 0
 }
---
+++ pkg/main/day/day3_test.go
@@ -0,0 +1,30 @@
+package day
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const input1 = "vJrwpWtwJgWrhcsFMMfFFhFp"
+const input2 = "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL"
+const input3 = "PmmdzqPrVvPwwTWBwg"
+const input4 = "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn"
+const input5 = "ttgJtRGJQctTZtZT"
+const input6 = "CrZsJsPPZsGzwwsLwLmpwMDw"
+
+func Test_share(t *testing.T) {
+	d := Day3{}
+
+	inp1 := d.processInput([]string{input1})
+	assert.Equal(t, 'p', inp1[0].share(), "is p")
+
+	inp2 := d.processInput([]string{input2})
+	assert.Equal(t, 'L', inp2[0].share(), "is p")
+
+}
+
+func Test_priority(t *testing.T) {
+	assert.Equal(t, 16, priority('p'))
+	assert.Equal(t, 38, priority('L'))
+}
